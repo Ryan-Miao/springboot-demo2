248833f3c80748a7866c3a9d3fcedf2ebecd05cb
ryan <v-rmiao@expedia.com>
2017-11-16 17:37:34 +0800

    Initial commit

---
+++ src/main/java/com/test/demo/Application.java
@@ -0,0 +1,14 @@
+package com.test.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Ryan Miao on 11/16/17.
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ src/main/java/com/test/demo/controller/HelloController.java
@@ -0,0 +1,19 @@
+package com.test.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Created by Ryan Miao on 11/16/17.
+ */
+@Controller
+public class HelloController {
+
+    @ResponseBody
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    public String hello(){
+        return "{\"hello\":\"world\"}";
+    }
+}
